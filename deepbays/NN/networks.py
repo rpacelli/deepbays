6d82f5c762b2dd148f4b941608686c07bab1b46c
Rosalba Pacelli <rosalba.pacelli@gmail.com>
2024-10-03 11:29:37 +0200

    convolutional model now debugged

--- deepbays/NN/networks.py
+++ deepbays/NN/networks.py
@@ -68,43 +68,51 @@ class FCNet:
         print(f'\nThe network has {self.L} dense hidden layer(s) of size {self.N1} with {act_func} actviation function', sequential)
         return sequential
     
-class ConvNet(nn.Module):
-    def __init__(self, N0, Nc, mask = 7, stride = 7, padding = 1):
-        super(ConvNet, self).__init__()
-        self.N0 = N0 
-        self.Nc = Nc
-        self.mask = mask
-        self.stride = stride
-        self.padding = padding 
+class ConvNet:
+    def __init__(self, K, mask, L):
+        self.K = K
+        self.mask = mask #mask
+        self.L = L
 
-    def Sequential(self, bias, act_func):
+    def Sequential(self, input_channels, input_size, stride, bias, act_func):
         if act_func == "relu":
             act = nn.ReLU()
         elif act_func == "erf":
             act = Erf()
-        elif act_func == "id":
-            act = Id()
-        
         modules = []
         # First convolutional layer
-        first_layer = nn.Conv2d(self.N0, self.Nc, kernel_size=self.mask, stride=self.stride, padding=self.padding, bias=bias)
+        first_layer = nn.Conv2d(input_channels, self.K, self.mask, stride = stride, bias=bias)
         init.normal_(first_layer.weight, std=1)
         if bias:
             init.constant_(first_layer.bias, 0)
-        modules.append(Norm(np.sqrt(self.mask)))
-        modules.append(first_layer)
+        modules.extend([Norm(self.mask), first_layer, act])
+        # Calculate the output size of the last convolutional layer to determine the number of input features for the fully connected layer
+        # Assume no padding 
+        conv_output_size = np.sqrt(input_size)
+        conv_output_size = (conv_output_size - self.mask) // stride + 1
+        # Additional L-1 convolutional layers
+        for l in range(self.L - 1):
+            layer = nn.Conv2d(self.K, self.K, self.mask,stride = stride, bias=bias) 
+            init.normal_(layer.weight, std=1)
+            conv_output_size = (conv_output_size - self.mask) // stride + 1
+            if bias:
+                init.constant_(layer.bias, 0)
+            modules.extend([Norm(np.sqrt(conv_output_size)),layer, act])
+        # Flatten the tensor before the fully connected layer
         modules.append(nn.Flatten())
-
-        self.N1 = (self.mask**2)*self.Nc 
-        modules.append(act)
-        modules.append(Norm(np.sqrt(self.N1)))
-        # Last layer
-        last_layer = nn.Linear(self.N1, 1, bias=bias)  
-        init.normal_(last_layer.weight, std=1) 
+        with torch.no_grad(): 
+            dummy_net_sequential = nn.Sequential(*modules)
+            out = dummy_net_sequential(torch.randn(int(np.sqrt(input_size)),int(np.sqrt(input_size))).unsqueeze(0))
+            FC_params = len(out.flatten())    
+        print(f"Number of parameters in the fully connected layer: {FC_params * input_channels}")
+        # Fully connected layer
+        last_layer = nn.Linear(FC_params, 1, bias=bias)
+        init.normal_(last_layer.weight, std=1)
         if bias:
-            init.normal_(last_layer.bias, std=1)
+            init.constant_(last_layer.bias, 0)
+        modules.append(Norm(np.sqrt(FC_params)))
         modules.append(last_layer)
-
         sequential = nn.Sequential(*modules)
-        print(f'\nThe network has 1 convolutional hidden layer(s) of size {self.N1} with {act_func} activation function', sequential)
+        print(f'\nThe network has {self.L} convolutional hidden layer(s) with {self.K} kernels of size {self.f} and {act_func} activation function', sequential)
         return sequential
+
