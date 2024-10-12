341b6fcde4d181d9d313fc343d71f0228c853ac3
Rosalba Pacelli <rosalba.pacelli@gmail.com>
2024-10-12 16:13:38 +0200

    removed function still being tested

--- deepbays/rkgp/__init__.py
+++ deepbays/rkgp/__init__.py
@@ -1,5 +1,5 @@
 from .theory_1HL_FC_single_output import FC_1HL, FC_1HL_corrected, FC_1HL_nonodd
 from .theory_1HL_FC_multiple_output import FC_1HL_multiclass
-from .theory_1HL_extras import FC_1HL_spectral, FC_1HL_multitask
 from .theory_deep_FC import FC_deep
 from .theory_1HL_CONV import CONV_1HL, validation_CONV_1HL
+#from .theory_1HL_extras import FC_1HL_spectral, FC_1HL_multitask
