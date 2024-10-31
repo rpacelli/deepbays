e3ab66b488b7d7286b28e2eb955f47c604c1b2d1
Rosalba Pacelli <rosalba.pacelli@gmail.com>
2024-10-31 12:03:44 +0100

    trying to parallelize on GPUS

--- deepbays/rkgp/theory_1HL_FC_single_output_cuda.py
+++ deepbays/rkgp/theory_1HL_FC_single_output_cuda.py
@@ -2,9 +2,9 @@ import cupy as cp
 from scipy.optimize import minimize
 from scipy.optimize import fsolve
 from .. import kernels
+from cupy.cuda import Device
 import torch
 
-## DEBUGGED VERSION, PASSED ALL TESTS 01/07/2024
 class FC_1HL_cuda():
     def __init__(self, 
                  N1   : int, 
@@ -12,67 +12,75 @@ class FC_1HL_cuda():
                  l0   : float = 1.0,
                  l1   : float = 1.0,
                  act  : str = "erf", 
-                 bias : bool = False):
+                 bias : bool = False,
+                 device_id: int = 0):
+        self.device = Device(device_id)
         self.N1, self.l0, self.l1, self.T = N1, l0, l1, T
         self.kernel = eval(f"kernels.kernel_{act}_cuda")
         if bias: 
             self.kernel = eval(f"kernels.kernel_{act}_bias_cuda")
 
     def effectiveAction(self, x):
-        A = self.T * cp.identity(self.P) + (x/self.l1) * self.diagK
-        invA = cp.linalg.inv(A)
-        return ( x - cp.log(x)
-            + (1/self.N1) * cp.sum(cp.log(self.T + x * self.eigvalK / self.l1))
-            + (1/self.N1) * cp.dot(self.yT, cp.dot(invA, self.yT)) )
+        with self.device:
+            A = self.T * cp.identity(self.P) + (x/self.l1) * self.diagK
+            invA = cp.linalg.inv(A)
+            return ( x - cp.log(x)
+                + (1/self.N1) * cp.sum(cp.log(self.T + x * self.eigvalK / self.l1))
+                + (1/self.N1) * cp.dot(self.yT, cp.dot(invA, self.yT)) )
 
     def optimize(self, x0 = 1.): # x0 is initial condition
-        optQ = minimize(self.effectiveAction, x0, bounds = ((1e-8,cp.inf),) , tol=1e-12)
-        self.optQ = (optQ.x).item()
-        assert self.optQ > 0 , "Unphysical solution found (Q is negative)."
+        with self.device:
+            optQ = minimize(self.effectiveAction, x0, bounds = ((1e-8,cp.inf),) , tol=1e-12)
+            self.optQ = (optQ.x).item()
+            assert self.optQ > 0 , "Unphysical solution found (Q is negative)."
 
     def setIW(self):
         self.optQ = 1
 
     def preprocess(self, Xtrain, Ytrain):
-        self.Xtrain = cp.asarray(Xtrain)
-        self.Ytrain = cp.asarray(Ytrain)
-        self.P, self.N0 = self.Xtrain.shape
-        self.corrNorm = 1/(self.N0*self.l0)
-        self.C = cp.dot(self.Xtrain, self.Xtrain.T) * self.corrNorm        
-        #self.Xtrain = Xtrain
-        #self.Ytrain = Ytrain
-        self.CX = self.C.diagonal()
-        self.K = kernels.computeKmatrix(self.C, self.kernel)
-        self.eigvalK, eigvecK = cp.linalg.eigh(self.K)
-        self.diagK = cp.diagflat(self.eigvalK)
-        self.Udag = eigvecK.T
-        self.yT = cp.matmul(self.Udag, self.Ytrain.squeeze())
+        with self.device:
+            self.Xtrain = cp.asarray(Xtrain)
+            self.Ytrain = cp.asarray(Ytrain)
+            self.P, self.N0 = self.Xtrain.shape
+            self.corrNorm = 1/(self.N0*self.l0)
+            self.C = cp.dot(self.Xtrain, self.Xtrain.T) * self.corrNorm        
+            #self.Xtrain = Xtrain
+            #self.Ytrain = Ytrain
+            self.CX = self.C.diagonal()
+            self.K = kernels.computeKmatrix(self.C, self.kernel)
+            self.eigvalK, eigvecK = cp.linalg.eigh(self.K)
+            self.diagK = cp.diagflat(self.eigvalK)
+            self.Udag = eigvecK.T
+            self.yT = cp.matmul(self.Udag, self.Ytrain.squeeze())
 
     def computeTestsetKernels(self, Xtest):
-        self.Ptest = len(Xtest)
-        self.Xtest = cp.asarray(Xtest)
-        self.C0 = cp.dot(self.Xtest, self.Xtest.T).diagonal() * self.corrNorm
-        self.C0X = cp.dot(self.Xtest, self.Xtrain.T) * self.corrNorm
-        self.K0 =  self.kernel(self.C0, self.C0, self.C0) 
-        self.K0X = self.kernel(self.C0[:,None], self.C0X, self.CX[None, :])
+        with self.device:
+            self.Ptest = len(Xtest)
+            self.Xtest = cp.asarray(Xtest)
+            self.C0 = cp.dot(self.Xtest, self.Xtest.T).diagonal() * self.corrNorm
+            self.C0X = cp.dot(self.Xtest, self.Xtrain.T) * self.corrNorm
+            self.K0 =  self.kernel(self.C0, self.C0, self.C0) 
+            self.K0X = self.kernel(self.C0[:,None], self.C0X, self.CX[None, :])
     
     def predict(self, Xtest):
-        self.computeTestsetKernels(Xtest)
-        self.orderParam = self.optQ / self.l1
-        A = self.orderParam * self.K + (self.T) * cp.eye(self.P)
-        invK = cp.linalg.inv(A)
-        self.rK0X = self.orderParam * self.K0X
-        self.K0_invK = cp.matmul(self.rK0X, invK)
-        self.Ypred =  cp.dot(self.K0_invK, self.Ytrain)
-        return self.Ypred
+        with self.device:
+            self.computeTestsetKernels(Xtest)
+            self.orderParam = self.optQ / self.l1
+            A = self.orderParam * self.K + (self.T) * cp.eye(self.P)
+            invK = cp.linalg.inv(A)
+            self.rK0X = self.orderParam * self.K0X
+            self.K0_invK = cp.matmul(self.rK0X, invK)
+            self.Ypred =  cp.dot(self.K0_invK, self.Ytrain)
+            return self.Ypred
     
     def averageLoss(self, Ytest):
-        self.Ytest = cp.asarray(Ytest)
-        self.rK0 = self.orderParam * self.K0 
-        bias = self.Ytest - self.Ypred 
-        var = self.rK0 - cp.sum(self.K0_invK * self.rK0X, axis=1)
-        predLoss = bias**2 + var 
-        return predLoss.mean().item(), (bias**2).mean().item(), var.mean().item()
+        with self.device:
+            self.Ytest = cp.asarray(Ytest)
+            self.rK0 = self.orderParam * self.K0 
+            bias = self.Ytest - self.Ypred 
+            var = self.rK0 - cp.sum(self.K0_invK * self.rK0X, axis=1)
+            predLoss = bias**2 + var 
+            return predLoss.mean().item(), (bias**2).mean().item(), var.mean().item()
 
 ## FROM HERE  
 class FC_1HL_corrected():
