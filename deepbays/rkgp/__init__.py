347a0f6249be4a6cd467777960f4207fbe1ba979
Rosalba Pacelli <rosalba.pacelli@gmail.com>
2025-03-01 11:15:12 +0100

    commented experimental cuda code requiring cupy package

--- deepbays/rkgp/__init__.py
+++ deepbays/rkgp/__init__.py
@@ -1,5 +1,5 @@
 from .theory_1HL_FC_single_output import FC_1HL, FC_1HL_corrected, FC_1HL_nonodd
-from .theory_1HL_FC_single_output_cuda import FC_1HL_cuda
+#from .theory_1HL_FC_single_output_cuda import FC_1HL_cuda
 from .theory_1HL_FC_multiple_output import FC_1HL_multiclass
 from .theory_deep_FC import FC_deep
 from .theory_1HL_CONV import CONV_1HL, validation_CONV_1HL
