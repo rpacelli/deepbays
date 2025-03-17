ed0a1ee253c5531aa8544337f37abfa87763f0e1
Paolo Baglioni <paolo@Paolos-MacBook-Pro.local>
2025-03-17 10:56:27 +0100

    added vanilla renormalization to deep fc - to be checked

--- deepbays/rkgp/__init__.py
+++ deepbays/rkgp/__init__.py
@@ -1,6 +1,7 @@
 from .theory_1HL_FC_single_output import FC_1HL, FC_1HL_corrected, FC_1HL_nonodd
 #from .theory_1HL_FC_single_output_cuda import FC_1HL_cuda
 from .theory_1HL_FC_multiple_output import FC_1HL_multiclass
-from .theory_deep_FC import FC_deep
+from .theory_deep_FC_nested import FC_deep_nested
+from .theory_deep_FC_vanilla import FC_deep_vanilla
 from .theory_1HL_CONV import CONV_1HL, validation_CONV_1HL
 #from .theory_1HL_extras import FC_1HL_spectral, FC_1HL_multitask
