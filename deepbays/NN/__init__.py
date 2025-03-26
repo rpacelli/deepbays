4c95202c440db6ac83a182f4091b915d14b011a9
Paolo Baglioni <paolo@Paolos-MacBook-Pro.local>
2025-03-26 19:00:35 +0100

    added import training_maybe_faster to __init__,  training_maybe_faster still to be debugged

--- deepbays/NN/__init__.py
+++ deepbays/NN/__init__.py
@@ -2,5 +2,6 @@ from .training import train, regLoss
 #from .multitask_training import multitaskMSE, multitaskTrain, multitaskLoss
 from .training import test
 from .training import LangevinOpt
+from .training_maybe_faster import LangevinOpt
 from .networks import FCNet
 from .networks import ConvNet
