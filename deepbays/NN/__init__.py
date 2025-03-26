a49b4263105359abafa4f47a86f9e18738459fe5
Paolo Baglioni <paolo@Paolos-MacBook-Pro.local>
2025-03-26 23:58:32 +0100

    added v2 of maybe faster MCMC with __init__; to be checked; (second commit for include __init__)

--- deepbays/NN/__init__.py
+++ deepbays/NN/__init__.py
@@ -3,5 +3,6 @@ from .training import train, regLoss
 from .training import test
 from .training import LangevinOpt
 from .training_maybe_faster import LangevinOpt
+from .training_maybe_faster_v2 import LangevinOpt
 from .networks import FCNet
 from .networks import ConvNet
