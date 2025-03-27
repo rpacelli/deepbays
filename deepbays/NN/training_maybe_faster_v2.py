a636a6e32d9823b631e0cb8ae66902caf2b8b085
Paolo Baglioni <paolo@Paolos-MacBook-Pro.local>
2025-03-27 11:36:06 +0100

    modified lang opt to include light for loops - to be checked

--- deepbays/NN/training_maybe_faster_v2.py
+++ deepbays/NN/training_maybe_faster_v2.py
@@ -8,19 +8,23 @@ from torch import nn
 from torch.optim import Optimizer
 
 class LangevinOpt(optim.Optimizer):
-    def __init__(self, model: nn.Module, lr, temperature):
+    def __init__(self, model: nn.Module, lr, temperature, priors):
         defaults = {'lr': lr, 'temperature': temperature}
         super().__init__(model.parameters(), defaults)
+        for group, lambda_j in zip(self.param_groups, priors):
+            group['lambda'] = lambda_j
+            group['noise_std'] = (2 * group['lr'] * group['temperature']) ** 0.5
 
     @torch.no_grad()
     def step(self, closure=None):
         for group in self.param_groups:
-            lr, T = group['lr'], group['temperature']
-            noise_std = (2 * lr * T) ** 0.5
             for param in group['params']:
                 if param.grad is not None:
-                    param.add_(-lr * (param.grad + T * param) + torch.randn_like(param) * noise_std)
+                    param.add_( -group['lr'] * (param.grad + group['temperature'] * group['lambda'] * param) + torch.randn_like(param) * group['noise_std'] )
 
+
+
+    
 def test(net, test_data, test_labels, criterion):
         net.eval()
         with torch.no_grad():
@@ -35,11 +39,10 @@ def computeNormsAndOverlaps(Snet,Tnet):
     overlap = torch.dot(Snet[i].weight.flatten(),Tnet[i].weight.flatten())
     return Snorm, Tnorm, overlap
 
-def train(net, data, labels, criterion, optimizer, T, priors):
+def train(net, data, labels, criterion, optimizer):
     net.train()
     optimizer.zero_grad(set_to_none=True)
-    outputs = net(data)
-    loss = criterion(outputs, labels)
+    loss = criterion(net(data), labels)
     loss.backward()
     optimizer.step()
 
