245695a6de21206ba961fa7943847aadddcf2866
ckeup <christian.keup@rwth-aachen.de>
2025-07-04 13:05:56 +0200

    add initial version of poker hand task

--- deepbays/tasks/synthetic_tasks.py
+++ deepbays/tasks/synthetic_tasks.py
@@ -64,3 +64,74 @@ class synthetic_1hl_dataset:
         
         return inputs, targets.unsqueeze(1), test_inputs, test_targets.unsqueeze(1)
     
+
+
+class pokerhand_dataset:
+    """Todo: currently add function do download and preprocess data, currently need to ask CK to get the data files """
+    
+    def __init__(
+        self,
+        datadir: str = '/home/keup/torchvision/poker/',
+        labels: list = None
+        ):
+        self.labels = labels
+        self.train_file = datadir + 'train_reducedpoker.pt'
+        self.test_file = datadir + 'test_reducedpoker.pt'
+        
+
+    def load_poker_data(
+        self,
+        pt_file: str,
+        nsamples: int = -1,
+        labels: list = None,
+        regression: bool = True
+        ):
+        """
+        Loads a .pt file saved by prepare_poker_data and returns a torch.Tensors X and y,
+        with options to restrict number of samples and subset of labels (their values will be remapped to 0, 1, 2,...).
+    
+        Args:
+            pt_file: path to the .pt file containing (features, labels).
+            nsamples: number of samples to include (after label filtering). Use -1 for all.
+            labels: list of class labels to include (e.g. [1,3,4]). Use None for all classes.
+            regression: if True, return the labels as float in shape (nsamples, 1), 
+                        if False, return the labels as long int in shape (nsamples), for use with crossentropy loss
+    
+        Returns:
+            X: Tensor of shape (nsamples, 10)
+            y: Tensor of shape (nsamples, 1)
+        """
+        X, y = torch.load(pt_file)
+    
+        # Filter by labels if provided
+        if labels is not None:
+            mask = torch.zeros_like(y, dtype=torch.bool)
+            for lbl in labels:
+                mask |= (y == lbl)
+            X = X[mask]
+            y = y[mask]
+            
+            # remap labels to 0, 1, 2 ...
+            new_y = torch.empty_like(y)
+            for new_idx, orig_lbl in enumerate(labels):
+                new_y[y == orig_lbl] = new_idx
+            y = new_y
+    
+        # Restrict number of samples
+        if nsamples > 0:
+            X = X[:nsamples]
+            y = y[:nsamples]
+        
+        if regression:
+            return X, y.unsqueeze(1).float()
+        else:
+            return X, y
+    
+    def make_data(self, P, Pt):
+        inputs, targets = self.load_poker_data(self.train_file, nsamples=P, labels=self.labels, regression=True)
+        test_inputs, test_targets = self.load_poker_data(self.test_file, nsamples=Pt, labels=self.labels, regression=True)
+        return inputs, targets, test_inputs, test_targets
+    
+    
+
+    
